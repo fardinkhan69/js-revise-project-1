c0ceb46a9ca957773ae8bc3fb0a7bf676dcbdab3
Fardin Khan <fardinofficial69@gmail.com>
2025-04-04 19:16:22 +0600

    generated html for cart summary

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,50 +1,14 @@
-const products = [
-    {
-        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-        rating: {
-          stars: 4.5,
-          count: 87
-        },
-        priceCents: 1090,
-        keywords: [
-          "socks",
-          "sports",
-          "apparel"
-        ]
-      },
-      {
-        id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        image: "images/products/intermediate-composite-basketball.jpg",
-        name: "Intermediate Size Basketball",
-        rating: {
-          stars: 4,
-          count: 127
-        },
-        priceCents: 2095,
-        keywords: [
-          "sports",
-          "basketballs"
-        ]
-      },
-      {
-        image:"images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-        name:'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating:{
-          stars: 4.5,
-          count: 56
-        },
-        priceCents: 795
-      }
-];
+import { cart,addToCart } from '../data/cart.js';
+import { products } from '../data/products.js';
 
 let productsHTML = '';
 
 
+
+
 products.forEach((proudct) => {
     //extracting the values from the object
-    const {image,name,rating,priceCents} = proudct;
+    const { image, name, rating, priceCents, id } = proudct;
 
     productsHTML += `
         <div class="product-container">
@@ -59,14 +23,14 @@ products.forEach((proudct) => {
 
             <div class="product-rating-container">
                 <img class="product-rating-stars"
-                src="images/ratings/rating-${rating.stars*10}.png">
+                src="images/ratings/rating-${rating.stars * 10}.png">
                 <div class="product-rating-count link-primary">
                 ${rating.count}
                 </div>
             </div>
 
             <div class="product-price">
-                $${(priceCents/100).toFixed(2)}
+                $${(priceCents / 100).toFixed(2)}
             </div>
 
             <div class="product-quantity-container">
@@ -91,15 +55,45 @@ products.forEach((proudct) => {
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${id}">
+            
                 Add to Cart
             </button>
             </div>
     `;
-    
+
 });
 
 
 const productsGrid = document.querySelector('.js-products-grid');
 
 productsGrid.innerHTML = productsHTML;
+
+
+
+function updateCartQuantity() {
+    let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+    })
+
+    document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+}
+
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+        button.addEventListener('click', () => {
+            //add the product to the cart
+            const productId = button.dataset.productId; // Corrected variable name
+            //check if the product is already in the cart
+
+            addToCart(productId);
+
+            updateCartQuantity();
+
+
+
+        })
+    })
